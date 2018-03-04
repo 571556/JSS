169bb60603a788ae3200ec031caae8c06eb42e95
571556 <31258700+571556@users.noreply.github.com>
2018-03-04 22:46:11 +0100

    ArrayLists

--- src/jsjf/ArrayUnorderedList.java
+++ src/jsjf/ArrayUnorderedList.java
@@ -1,5 +1,5 @@
 package jsjf;
-import java.util.Iterator;
+import jsjf.exception.*;
 
 public class ArrayUnorderedList<T> extends ArrayList<T> implements UnorderedListADT<T>{
 
@@ -9,20 +9,59 @@ public class ArrayUnorderedList<T> extends ArrayList<T> implements UnorderedList
 	
 	@Override
 	public void addToFront(T element) {
-		// TODO Auto-generated method stub
+		
+		if (isEmpty())
+			list[0] = element;
+		
+		for(int i = rear; i > 0; i--) {
+			list[i] = list[i - 1];
+		}
+		list[0] = element;
+		rear++;
+		modCount++;
 		
 	}
 
 	@Override
 	public void addToRear(T element) {
-		// TODO Auto-generated method stub
+		
+		list[rear] = element;
+		rear++;
+		modCount++;
 		
 	}
 
 	@Override
-	public void addAfter(T element, T target) {
-		// TODO Auto-generated method stub
+	public void addAfter(T element, T target) throws ElementNotFoundException{
 		
+		if (size() == list.length)
+			expandCapacity();
+		
+		int scan = 0;
+		
+		while (scan < rear && !target.equals(list[scan]))
+			scan++;
+		
+		if (scan == rear)
+			throw  new ElementNotFoundException("UnorderedList");
+		
+		for (int shift = rear; shift > scan; shift--)
+			list[shift] = list[shift-1];
+		
+		list[scan] = element;
+		rear++;
+		modCount++;
 	}
+	
+	private void expandCapacity() {
+		T[] nevv = (T[])(new Object[list.length * 2]);
+		
+		for ( int i = 0; i < nevv.length; i++) {
+			nevv[i] = list[i];
+		}
+		list = nevv;
+	}
+	
+	
 
 }
