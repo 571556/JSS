cb8eb13389e9cce1da1103c2557b28947adb0833
571556 <31258700+571556@users.noreply.github.com>
2018-03-04 22:43:07 +0100

    ArrayLists

---
+++ src/jsjf/ArrayUnorderedList.java
@@ -0,0 +1,28 @@
+package jsjf;
+import java.util.Iterator;
+
+public class ArrayUnorderedList<T> extends ArrayList<T> implements UnorderedListADT<T>{
+
+	public ArrayUnorderedList() {
+		super();
+	}
+	
+	@Override
+	public void addToFront(T element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addToRear(T element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addAfter(T element, T target) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
