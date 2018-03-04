6e02565f14a0055e237269f77502d16a2dd1aa27
571556 <31258700+571556@users.noreply.github.com>
2018-03-04 22:47:23 +0100

    a

--- src/jsjf/LinkedList.java
+++ src/jsjf/LinkedList.java
@@ -1,5 +1,14 @@
 package jsjf;
 
-public abstract class LinkedList<T> impl{
+public abstract class LinkedList<T> implements ListADT<T>, Iterable<T>{
 
+	protected int count;
+	protected DoublyLinearNode<T> head, tail;
+	protected int modCount;
+	
+	public LinkedList() {
+		count = modCount = 0;
+		head = tail = null;
+	}
+	
 }
