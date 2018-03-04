011beee352fa1db09cc167c1e8a020ab7ba6dab4
571556 <31258700+571556@users.noreply.github.com>
2018-03-05 00:46:53 +0100

    LinkedList

--- src/jsjf/LinkedList.java
+++ src/jsjf/LinkedList.java
@@ -1,5 +1,8 @@
 package jsjf;
 
+import java.util.Iterator;
+import jsjf.exception.*;
+
 public abstract class LinkedList<T> implements ListADT<T>, Iterable<T>{
 
 	protected int count;
@@ -10,5 +13,89 @@ public abstract class LinkedList<T> implements ListADT<T>, Iterable<T>{
 		count = modCount = 0;
 		head = tail = null;
 	}
+
+	@Override
+	public T removeFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T removeLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T remove(T element) throws EmptyCollectionException, ElementNotFoundException{
+		if(isEmpty())
+			throw new EmptyCollectionException("LinkedList");
+		
+		boolean found = false;
+		DoublyLinearNode<T> previous = null;
+		DoublyLinearNode<T> current = head;
+		
+		while ( current != null && !found)
+			if(element.equals(current.getElement()))
+				found = true;
+			else {
+				previous = current;
+				current = current.getNext();
+			}
+		if (!found)
+			throw new ElementNotFoundException("LinkedList");
+		
+		if (size() == 1)
+			head = tail = null;
+		else if (current.equals(head))
+			head = current.getNext();
+		else if (current.equals(tail)) {
+			tail = previous;
+			tail.setNext(null);
+		}
+		else
+			previous.setNext(current.getNext());
+		count--;
+		modCount++;
+		
+		return current.getElement();
+	}
+
+	@Override
+	public T first() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T last() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contains(T target) {
+		
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 	
 }
