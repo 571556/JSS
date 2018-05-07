768e748de81f4da48080746816dc30f84ba84483
571556 <31258700+571556@users.noreply.github.com>
2018-05-07 13:36:31 +0200

    sfs

--- src/ch8/Palindrome.java
+++ src/ch8/Palindrome.java
@@ -4,29 +4,22 @@ public class Palindrome {
 
 	public static void main(String[] args) {
 		
-		System.out.println(palindrom("hallo"));
+		System.out.println(palindrom("deded"));
 
 	}
 	
+	
+	
 	private static boolean palindrom(String s) {
 		boolean result = false;
-		String reverse = reverse(s);
-		if()
-			
-			
-	}
-	
-	private static void palindrom(String s,String d) {
 		
-	}
-	
-	private static String reverse(String s) {
-		String result;
-		
-		if(s.length() == 1) 
-			result = s;
-		else
-			result = s.charAt(s.length()-1) + reverse(s.substring(0, s.length()-1));
+		if(s.length() <= 2) {
+			if(s.length() == 1)
+				result = true;
+			else if(s.length() == 2)
+				result = s.charAt(0) == s.charAt(1);
+		}else
+			result = s.charAt(0) == s.charAt(s.length()-1) && palindrom(s.substring(1, s.length()-1));
 		
 		return result;
 	}
